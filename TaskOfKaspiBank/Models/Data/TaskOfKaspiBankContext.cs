7efe67bffd992554c29f5523e34080aff1fc1056
Almat <seyderaly98@gmail.com>
2021-12-18 16:17:41 +0600

    #1 Проект подготовлен к работе

---
+++ TaskOfKaspiBank/Models/Data/TaskOfKaspiBankContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskOfKaspiBank.Models.Data
+{
+    public class TaskOfKaspiBankContext: IdentityDbContext<IdentityUser>
+    {
+        public TaskOfKaspiBankContext(DbContextOptions<TaskOfKaspiBankContext> options) : base(options) {}
+    }
+}
--- TaskOfKaspiBank/Startup.cs
+++ TaskOfKaspiBank/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TaskOfKaspiBank.Models.Data;
 
 namespace TaskOfKaspiBank
 {
@@ -23,6 +25,8 @@ namespace TaskOfKaspiBank
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TaskOfKaspiBankContext>(options => options.UseNpgsql(connection,o => o.SetPostgresVersion(9, 6)));
             services.AddControllersWithViews();
         }
 
