1d3d681ce46e9bd8ba79a84c6919890a033ab575
Almat <seyderaly98@gmail.com>
2021-12-19 15:31:30 +0600

    #3 Добавил: 4. Кнопка "Выполнен" - изменяет состояние заказа на Выполнен

--- TaskOfKaspiBank/Controllers/ManagerController.cs
+++ TaskOfKaspiBank/Controllers/ManagerController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskOfKaspiBank.Models.Data;
+using TaskOfKaspiBank.Models.Enums;
 
 namespace TaskOfKaspiBank.Controllers
 {
@@ -50,6 +51,30 @@ namespace TaskOfKaspiBank.Controllers
             }
         }
 
+        /// <summary>
+        /// Отправить заказ 
+        /// </summary>
+        /// <param name="orderId">Id заказа</param>
+        [HttpPost]
+        public async Task<IActionResult> SendOrder(string orderId)
+        {
+            try
+            {
+                if (!await _db.Orders.AnyAsync(o => o.Id == orderId)) return Json(false);
+                var order = await _db.Orders.FirstOrDefaultAsync(o => o.Id == orderId);
+                order.Status = OrderStatus.Completed;
+                await _db.SaveChangesAsync();
+                return Json(true);
+            }
+            catch (Exception e)
+            {
+#if(DEBUG)
+                //TODO: Логирование 
+                Console.WriteLine(e);
+#endif
+                return StatusCode(500);
+            }
+        }
         #endregion
         
     }
